90a9df0f62e0c77a69e01c9408a568abfa214cef
anisul <anis.2803@gmail.com>
2018-02-06 03:18:47 +0200

    new entity 'location' created

---
+++ src/main/java/com/perccom/csense/domain/Location.java
@@ -0,0 +1,142 @@
+package com.perccom.csense.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Location.
+ */
+@Entity
+@Table(name = "location")
+public class Location implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "meta")
+    private String meta;
+
+    @ManyToOne
+    private User user;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Location longitude(Double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Location latitude(Double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Location description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMeta() {
+        return meta;
+    }
+
+    public Location meta(String meta) {
+        this.meta = meta;
+        return this;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Location user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Location location = (Location) o;
+        if (location.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), location.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+            "id=" + getId() +
+            ", longitude='" + getLongitude() + "'" +
+            ", latitude='" + getLatitude() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", meta='" + getMeta() + "'" +
+            "}";
+    }
+}
